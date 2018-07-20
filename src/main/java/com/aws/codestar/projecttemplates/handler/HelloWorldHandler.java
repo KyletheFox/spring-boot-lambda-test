1e45a1514d8304a4ab89a4151dcbc3cdaf375f6b
kylethefox <halobuster1982>
2018-07-19 21:32:07 -0400

    reset resttemplate

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -3,8 +3,7 @@ package com.aws.codestar.projecttemplates.handler;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import com.amazonaws.services.lambda.runtime.Context;
@@ -16,19 +15,18 @@ import com.aws.codestar.projecttemplates.GatewayResponse;
  */
 public class HelloWorldHandler implements RequestHandler<Object, Object> {
 	
-	
-	static AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
-			HelloWorldHandler.class.getPackage().getName());
-
-	public HelloWorldHandler() {
-		 ctx.getAutowireCapableBeanFactory().autowireBean(this);
-	}
+//	
+//	static AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+//			HelloWorldHandler.class.getPackage().getName());
+//
+//	public HelloWorldHandler() {
+//		 ctx.getAutowireCapableBeanFactory().autowireBean(this);
+//	}
 
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        RestTemplate restTemplate = new RestTemplate();
-        System.out.println(restTemplate.toString());
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
         String joke = restTemplate.getForObject("https://api.chucknorris.io/jokes/random", String.class);
         return new GatewayResponse(joke, headers, 200);
     }
